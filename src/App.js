64dff1768649d48845cdb3b9b2ed20fd74d59f33
Atharva <deoatharva069@gmail.com>
2023-04-30 20:35:08 +0530

    [First-image-slider-commit]

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import { Login } from "./pages/login"
 import { Navbar } from './components/navbar';
+import ImageSlider from './components/Slider/ImageSlider';
+import { SliderData } from './components/Slider/SliderData';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 function App() {
   return (
@@ -9,7 +11,10 @@ function App() {
       <Router> {/* everything related to react query dom has to be here */}
       <Navbar />{/* navbar component has to be placed between the Router and the Routes compulsorily! */}
         <Routes>
+        <Route path="/" element={<ImageSlider slides={SliderData} />} />
+        
         <Route path="/login" element={<Login />} />
+        
           {/* <Route path="/createcomment" element={<CreateComment />} /> */}
         </Routes>
       </Router>
---
+++ src/components/Layout/home.js
@@ -0,0 +1,4 @@
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import classes from "./MainNavigation.module.css";
+
